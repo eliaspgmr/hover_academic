0617af8714fd6e9bb9bf3b7e0affb11f75ae82f4
Elias Linux <elias.eliasferreira@gmail.com>
2019-01-04 21:16:59 -0300

    first commit on linux

--- src/sample/ConfigApp.java
+++ src/sample/ConfigApp.java
@@ -14,11 +14,11 @@ public class ConfigApp {
         Parent configRoot = FXMLLoader.load(getClass().getResource("view/Config.fxml"));
         configStage = new Stage();
         configStage.setTitle("Config");
-        configStage.setWidth(287);
+        configStage.setWidth(285);
         configStage.setHeight(342);
         configStage.setResizable(false);
         configStage.initOwner(stageOwner);
-        configStage.initModality(Modality.NONE);
+        configStage.initModality(Modality.APPLICATION_MODAL);
 
         configStage.setScene(new Scene(configRoot));
         configStage.show();
